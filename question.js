6c6ea07ef33e96f3001d4f02aeb2ded340ffd17c
Leticia Guerra <g.leticia@rocketmail.com>
2019-12-07 14:17:25 -0500

    Updating js and html start game and questions

--- question.js
+++ question.js
@@ -19,18 +19,57 @@ var title = "Commonly used data types DO NOT include:";
 var answer = "alerts";
 var title = answer
 var otherChoice = "false"
+
+
+
+var targetDiv = document.getElementById("quizwork");
+
+    targetDiv.textContent = "Commonly used data types DO NOT include";
+
+    var newDiv = document.createElement("div");
+    newDiv.textContent = myChoices;
+
+    targetDiv.appendChild(newDiv);
+
+
+
+document.getElementById("q1").addEventListener("click", button);
+function button() {
+  document.getElementById("q1").innerHTML = "false";
+}
+//
+document.getElementById("q2").addEventListener("click", button);
+function button() {
+  document.getElementById("q2").innerHTML = "false";
+}
+//
+document.getElementById("q3").addEventListener("click", button);
+
+function button() {
+  document.getElementById("q3").innerHTML = "true";
+}
+//
+document.getElementById("q4").addEventListener("click", button);
+
+function button() {
+  document.getElementById("q4").innerHTML = "false";
+}
+///
+
+
 // for ( interator ; condition ; incrementer )
-  for ( var i = 0 ; i < myQuestions.length; i = i + 1) {
-    console.log(myQuestions[i] + myAnswers[i]);
-  }
-   if ( title === answer ) {
-    alert ("true");
-  }
-    else {
-      alert("false")
-    }
+  //for ( var i = 0 ; i < myQuestions.length; i = i + 1) {
+   //console.log(myQuestions[i] + myAnswers[i]);
+ // }
+  // if ( title === answer ) {
+   // alert ("true");
+  //}
+    //else {
+     // alert("false")
+    //}
 
 
+    
       //document.getElementById("true").innerHTML = "True";
 
 
