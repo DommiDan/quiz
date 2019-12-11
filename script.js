1489697971a74ee95f549c8b90b0336c208132eb
Leticia Guerra <g.leticia@rocketmail.com>
2019-12-11 17:27:15 -0500

    Updated folder

---
+++ script.js
@@ -0,0 +1,19 @@
+//The first view of the application displays a button that starts the quiz.
+
+//Clicking the start button displays a series of questions.
+
+//Once the quiz begins, a timer starts.
+
+//If a question is answered incorrectly, additional time is subtracted from the timer.
+
+//The timer stops when all questions have been answered or the timer reaches 0.
+
+ //After the game ends, the user can save their initials and score to a highscores view using local storage.
+
+ document.getElementById("start").addEventListener("click", myFunction);
+
+ function myFunction() {
+   document.getElementById("start").innerHTML = result;
+ }
+ var demo = "Start Quiz";
+ var result = demo.link("startquiz.html");
